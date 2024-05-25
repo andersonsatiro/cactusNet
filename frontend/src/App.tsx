22f5612327903fd97f2a867d4f77a7dedc7daa1b
AndersonPaivaS <andersonpaiva135@gmail.com>
2024-05-25 06:19:45 -0300

    feat: create sidebar with navigation bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
-import { Button } from "./components/ui/button";
+import { Aside } from './components/home/Aside'
 
 export function App() {
   return (
-    <Button>Clique aqui</Button>
+    <div className="flex w-full h-full min-h-screen">
+      <Aside />
+
+      <main className='w-full bg-red-400'>
+        <span>sss</span>
+
+        <div>
+          <h1>skskk</h1>
+        </div>
+      </main>
+      
+    </div>
   )
 }
---
+++ frontend/src/components/home/Aside.tsx
@@ -0,0 +1,40 @@
+import { Button } from '../ui/button'
+import { Separator } from '../ui/separator'
+import { House, Users, CurrencyCircleDollar, Database  } from "@phosphor-icons/react";
+
+export function Aside() {
+  return(
+    <aside className='w-[12%] border-r-solid border-r-[1px] border-r-border'>
+
+      <h1 className='w-full text-center py-3 text-lg'>
+        cactus<strong>NET</strong>
+      </h1>
+
+      <Separator />
+
+      <div className='flex flex-col p-3 gap-2 mt-5'>
+        <Button variant={'outline'} className='flex gap-1'>
+          <House size={16} />
+          Home
+        </Button>
+
+        <Button variant={'ghost'} className='flex gap-1'>
+          <Users size={16} />
+          Clientes
+        </Button>
+
+        <Button variant={'ghost'} className='flex gap-1'>
+          <CurrencyCircleDollar  size={16} />
+          Financeiro
+        </Button>
+
+        <Button variant={'ghost'} className='flex gap-1'>
+          <Database  size={16} />
+          Consumo
+        </Button>
+
+      </div>
+
+    </aside>
+  )
+}
